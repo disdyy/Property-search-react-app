5c3451ca28b84398c66b4825c02e7df9b79be88b
Disadhi Ranasinghe <ranasinghedisadhi@gmail.com>
2026-01-02 14:56:47 +0530

    Simplify property cards by removing description from listings
    Removed property descriptions from the search results view to keep listings concise. Full descriptions are now shown only on the property details page for better UX.

--- property-search/src/Components/PropertyCard.jsx
+++ property-search/src/Components/PropertyCard.jsx
@@ -1,11 +1,6 @@
 import { Link } from "react-router-dom";
 
 export default function PropertyCard({ p, onAddFavourite }) {
-  const shortDesc =
-    p.description && p.description.length > 160
-      ? p.description.slice(0, 160) + "..."
-      : p.description;
-
   return (
     <div
       className="property-card"
@@ -20,13 +15,17 @@ export default function PropertyCard({ p, onAddFavourite }) {
 
       <div className="property-info">
         <div>
-          <div className="property-price">£{p.price.toLocaleString()}</div>
+          <div className="property-price">
+            £{p.price.toLocaleString()}
+          </div>
+
           <div className="property-meta">
             {p.bedrooms} bed • {p.type}
           </div>
 
-          <div className="property-desc">{shortDesc}</div>
-          <div className="property-location">{p.location}</div>
+          <div className="property-location">
+            {p.location}
+          </div>
         </div>
 
         <div className="property-actions">
@@ -34,7 +33,11 @@ export default function PropertyCard({ p, onAddFavourite }) {
             View property
           </Link>
 
-          <button className="btn" type="button" onClick={() => onAddFavourite(p.id)}>
+          <button
+            className="btn"
+            type="button"
+            onClick={() => onAddFavourite(p.id)}
+          >
             Add to favourites
           </button>
         </div>
